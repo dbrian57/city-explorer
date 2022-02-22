8e633e6f3dbb47bbd737bc738df53b3fae8b6fba
Daniel Brian <daniel.brian@gmail.com>
2022-02-22 16:28:59 -0500

    fixed form data problem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 class App extends React.Component {
 
@@ -8,33 +10,47 @@ class App extends React.Component {
     super(props);
     this.state = {
       city: '',
-      cityData: {}
+      cityData: {},
+      cityName: null,
+      cityLat: null,
+      cityLong: null
     }
   }
 
-handleSubmit = async (event) => {
-  event.preventDefault();
-  this.setState({
-    city: event.target.value
-  })
-   let locationData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=boston&format=json`);
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.sendRequest();
+  }
 
-   this.setState({
-     cityData: locationData
-   })
-   console.log(this.state.cityData.data[0].display_name);
+  sendRequest = async () => {
+    let locationData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
+    console.log(locationData);
+    this.setState({
+      cityData: locationData,
+      cityName: locationData.data[0].display_name,
+      cityLat: locationData.data[0].lat,
+      cityLong: locationData.data[0].lon
+    });
+    }
+
+  updateCity = (event) => {
+    this.setState({
+      city: event.target.value
+    })
   }
 
   render(){ 
-
     return (
       <>
-        <form>
-          <label>Enter a city: 
-            <input type="text" />
-          </label>
-          <button type="submit" onClick={this.handleSubmit}>Explore!</button>
-        </form>
+        <Form onSubmit={this.handleSubmit}>
+            <Form.Label>Enter a city to search for: </Form.Label>
+            <Form.Control onChange={this.updateCity} type="text" placeholder="Enter city" />
+            <Button type="submit">Explore City</Button>
+        </Form>
+
+        <p>{this.state.cityName}</p>
+        <p>{this.state.cityLong}</p>
+        <p>{this.state.cityLat}</p>
       </>
       );
     }
