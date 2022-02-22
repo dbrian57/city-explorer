9d4cf9f65ba69ab302a8711f268dc21d8358807a
Daniel Brian <daniel.brian@gmail.com>
2022-02-21 22:37:26 -0500

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 
 class App extends React.Component {
